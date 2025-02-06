c5dc51d1b51b8811232429ed44393bc0ae1810f9
Orçun Aşık <asikorcun@gmail.com>
2025-02-06 20:24:32 +0300

    Operators

---
+++ src/main/java/com/orcunasik/_1_part1_javatypes/_04_Operators.java
@@ -0,0 +1,23 @@
+package com.orcunasik._1_part1_javatypes;
+
+public class _04_Operators {
+    public static void main(String[] args) {
+        // operator
+        // +, -, *, /, %
+        // ++ 1 arttır
+        // -- 1 azalt
+        int number1 = 23, number2 = 7;
+        int addition = number1 + number2;
+        System.out.println("Toplam.: " + addition);
+        int subtraction = number1 - number2;
+        System.out.println("Çıkarma.: " + subtraction);
+        int multiplication = number1 * number2;
+        System.out.println("Çarpma.: " + multiplication);
+        int division = number1 / number2;
+        System.out.println("Bölme.: " + division);
+        int modulus = number1 % number2;
+        System.out.println("Kalan.: " + modulus);
+
+        //Mantıksal Operators: &&=VE ||=VEYA !=DEĞİL
+    }
+}
