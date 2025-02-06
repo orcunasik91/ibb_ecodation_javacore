2400ba694f299f0184067bc7bc936e7c9df5590b
Orçun Aşık <asikorcun@gmail.com>
2025-02-06 19:01:38 +0300

    primitive types

---
+++ src/main/java/com/orcunasik/_1_part1_javatypes/_02_JavaPrimitiveTypes.java
@@ -0,0 +1,30 @@
+package com.orcunasik._1_part1_javatypes;
+
+public class _02_JavaPrimitiveTypes {
+    public static void main(String[] args) {
+        //Primite Types : 8 tanedir. null değer alamaz.
+        //Tam Sayılar (4)
+        byte b1 = 127;
+        System.out.println("byte.: " + b1);
+        short s1 = 32767;
+        System.out.println("short.: " + s1);
+        int i1 = 2147483647;
+        System.out.println("int.: " + i1);
+        long l1 = 9223372036854775807L;
+        System.out.println("long.: " + l1);
+
+        //virgüllü sayılar(floating point)
+        float f2 = 3.14f;
+        System.out.println("float.: " + f2);
+        double d2 = 3.14;
+        System.out.println("double.: " + d2);
+
+        //boolean
+        boolean b3 = true;
+        System.out.println("boolean.: " + b3);
+
+        //char
+        char c4 = 'a';
+        System.out.println("char.: " + c4);
+    }
+}
