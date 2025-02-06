0c70ec0426d9708e858ab9250e6c04657696f450
Orçun Aşık <asikorcun@gmail.com>
2025-02-06 19:23:04 +0300

    variables

--- src/main/java/com/orcunasik/_1_part1_javatypes/_02_Variables.java
+++ src/main/java/com/orcunasik/_1_part1_javatypes/_02_Variables.java
@@ -3,24 +3,27 @@ package com.orcunasik._1_part1_javatypes;
 public class _02_Variables {
     public static void main(String[] args) {
         //Veri değişken isimlerini yazarken;
-        /*
-
-            2-) _ veya $ ile başlayabilirsiniz
-            3-) sayı ile bitebilir
-            4-) sayı ile başlanmaz
-            5-) _ veya $ dışında özel simgelerle başlanmaz
-         */
         //veri isimlendirmeleri;
         //1-) isim veya sıfat, zamir kullanmamız lazım.
         int schoolNumber = 34;
         System.out.println(schoolNumber);
         schoolNumber = 35;
         System.out.println(schoolNumber);
+
+        //2-) _ veya $ ile başlayabilirsiniz
         int _schoolNumber = 45;
         System.out.println(_schoolNumber);
         int $schoolNumber = 95;
         System.out.println($schoolNumber);
 
+        //3-) sayı ile bitebilir
+        int schoolNumber123 = 63;
+        System.out.println(schoolNumber123);
+        /*
+            4-) sayı ile başlanmaz
+            5-) _ veya $ dışında özel simgelerle başlanmaz
+            6-) değişken isimleri camelCase kuralına göre yazılır
+         */
         //yazamazsınız
         //int schoolNumber = 11; //aynı isimde yazamazsınız
         //int 44schoolNumber = 11; //sayıyla yazamazsınız
